474f7728fbc32fff4a428c31d13976b3c1a87e8d
lpastore <lpastore@20211367-AIT.prod.altengroup.dir>
2022-06-06 18:22:31 +0200

    Commit iniziale

---
+++ src/main/java/it/alten/tirocinio/HomepageController.java
@@ -0,0 +1,13 @@
+package it.alten.tirocinio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomepageController {
+	@RequestMapping({"", "/"})
+	public String getHomepage(Model model) {		
+		return "HomePage";
+	}
+}
---
+++ src/main/resources/static/js/W3Function.js
@@ -0,0 +1,25 @@
+/*
+ * Open Menu SideBar
+ */
+function w3_open() { document.getElementById("menuSidebar").style.display = "block"; }
+
+/*
+ * Close Menu SideBar
+ */			
+function w3_close() { document.getElementById("menuSidebar").style.display = "none"; }
+
+/*
+ * Opne the cascade sub menu for each item ov sidebar menu
+ */	
+function dropSubMenu(idDrop) {
+  	var x = document.getElementById(idDrop);
+  
+  	if (x.className.indexOf("w3-show") == -1) {
+    	x.className += " w3-show";
+    	x.previousElementSibling.className += " w3-blue";
+  	} else { 
+    	x.className = x.className.replace(" w3-show", "");
+    	x.previousElementSibling.className = 
+    	x.previousElementSibling.className.replace(" w3-blue", "");
+  	}
+}

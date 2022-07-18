886b3c79c30a02f8ff0be15c03c97f5c11de4587
lpastore <lpastore@20211367-AIT.prod.altengroup.dir>
2022-07-18 11:00:09 +0200

    bug fix

--- src/main/java/it/alten/tirocinio/services/LiquibaseEngineService.java
+++
@@ -1,8 +0,0 @@
-package it.alten.tirocinio.services;
-
-public interface LiquibaseEngineService {
-	/*
-	 * Method that allow to run the current open ChangeLog script
-	 */
-	boolean runChangeLogScript();
-}
